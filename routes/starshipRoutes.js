671f13db20ae3f7dfc054a8a078b455593e62479
Roger Campbell II <rc1336@gmail.com>
2019-01-28 15:20:42 -0500

    populated starshipRoutes.js

---
+++ routes/starshipRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const starShipController = require('../controllers/userControllers')
+//Need to create DB dir before bring this online
+// const mongoose = require('../db/connection')
+
+
+router.get('/', starShipController.index)
+
+router.get('/new', starShipController.new)
+
+router.post('/', starShipController.create)
+
+router.get('/:id', starShipController.show)
+
+router.get('/:id/edit', starShipController.edit)
+
+router.put('/:id', starShipController.update)
+
+router.get('/:id', starShipController.destroy)
+
+
+
+module.exports = router
