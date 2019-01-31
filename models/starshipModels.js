4684989b59b4af91abf37d0b272740770d9d8da2
Roger Campbell II <rc1336@gmail.com>
2019-01-31 09:33:42 -0500

    changing back to  starShipDBSetUp branch

--- models/starshipModels.js
+++ models/starshipModels.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const StarshipSchema = new mongoose.Schema({
-	name: String,
-	registry: String,
-	status: String,
-	dateStatus: String,
-	spacecraftClass: String,
-	rankingOfficers:{
-        // This info should come from the user model
-		name: {
-            firstName: String,
-            lastName: String,
-        },
-        ranks: String,
-    },
-    photos: String,
-    // This info should come from the user model
-})
+// const StarshipSchema = new mongoose.Schema({
+// 	name: String,
+// 	registry: String,
+// 	status: String,
+// 	dateStatus: String,
+// 	spacecraftClass: String,
+// 	rankingOfficers:{
+//         // This info should come from the user model
+// 		name: {
+//             firstName: String,
+//             lastName: String,
+//         },
+//         ranks: String,
+//     },
+//     photos: String,
+//     // This info should come from the user model
+// })
 
-mongoose.model('Starship', StarshipSchema)
+// mongoose.model('Starship', StarshipSchema)
 
 
-module.exports = mongoose
+// module.exports = mongoose
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = express.Router()
-//Need to create DB dir before bring this online
-// const mongoose = require('../db/connection')
+// const express = require('express')
+// const router = express.Router()
+// //Need to create DB dir before bring this online
+// // const mongoose = require('../db/connection')
 
 
-router.use('/user', require("./userRoutes"))
-// router.use('/starship', require("./starshipRoutes"))
-// router.use('/missionBrief', require("./missionRoutes"))
+// router.use('/user', require("./userRoutes"))
+// // router.use('/starship', require("./starshipRoutes"))
+// // router.use('/missionBrief', require("./missionRoutes"))
 
 
 
-module.exports = router
+// module.exports = router
